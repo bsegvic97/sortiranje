4805b3ad15f9cdde25dd473d8d2a28d8709af52e
bsegvic97 <62608075+bsegvic97@users.noreply.github.com>
2020-04-14 18:53:13 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# sortiranje
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+function sortiranje(a) {
+  a = a.split("");
+  a = a.sort();
+  a = a.join("");
+  return a;
+}
+console.log(sortiranje("webmaster"));
